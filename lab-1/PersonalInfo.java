a4c03db9b1e4c678e80d7cc74c3e5620f15bc09e
MD ATIKUR RAHAMAN <marh104660@gmail.com>
2025-12-10 01:41:28 +0600

    Add files via upload

---
+++ lab-1/PersonalInfo.java
@@ -0,0 +1,18 @@
+public class PersonalInfo {
+    public static void main(String[] args) {
+        String name = "Md Atikur Rahaman";
+        String studentId = "E251046";
+        int age = 21;
+        String email = "marh104660@gmail.com";
+        String phone = "+8801615374648";
+        String address = "Charandwip,Boalkhali, Chattogram";
+
+        System.out.println("Personal Information:");
+        System.out.println("Name: " + name);
+        System.out.println("Student ID: " + studentId);
+        System.out.println("Age: " + age);
+        System.out.println("Email: " + email);
+        System.out.println("Phone: " + phone);
+        System.out.println("Address: " + address);
+    }
+}
